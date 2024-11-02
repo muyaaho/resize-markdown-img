ddff7a4796c126a3e301d75a0f227fc90c29f1c8
muyaaho <skanthsu715@gmail.com>
2024-11-02 16:36:06 +0900

    feat: 자바스크립트 리팩토링

---
+++ script2.js
@@ -0,0 +1,38 @@
+const inputConatainer = document.querySelector('#inputCode');
+const sizeContainer = document.querySelector('#inputSize')
+const outputConatainer = document.querySelector('#outputCode');
+let size = 100;
+let inputCode = '';
+
+inputConatainer.addEventListener('input', function(e) {
+    inputCode = e.target.value;
+})
+
+sizeContainer.addEventListener('input', (e) => {
+    size = e.target.value;
+    outputConatainer.value = updateCode();
+})
+
+function resizeUrl(url, size) {
+    const re = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?[^)]/;
+
+    const result = url.match(re) ? url.match(re)[0] : "";
+    return `<img src=${result} width=${size}% height=${size}%/><br>`
+}
+
+function updateCode() {
+    const lines = inputCode.split('\n');
+    let result = '';
+
+    for (let line of lines) {
+        if (line.includes("![image]") || lines.includes("<img src=")) {
+            let prefix = line.includes("![image]") ? "![image]" : "<img src=";
+            const space = line.indexOf(prefix);
+
+            result += `${' '.repeat(space)}${resizeUrl(line,size)}\n`
+            continue;
+        }
+        result += `${line}\n`
+    }
+    return result;
+}
